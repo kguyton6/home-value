2b0549df2c929e0dc553162db747ddee8be4e9b2
kguyton6 <38778115+kguyton6@users.noreply.github.com>
2019-03-20 12:26:55 -0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  USERNAME: process.env.USERNAME,
+  USERNAME: process.env.EMAIL,
   PASSWORD: process.env.PASSWORD,
   port: process.env.SERVER_PORT
 };
