5c8b091859a4d2e03a555d466a83316ca8cd560b
kguyton6 <38778115+kguyton6@users.noreply.github.com>
2019-03-20 13:16:10 -0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  USERNAME: process.env.EMAIL,
+  EMAIL: process.env.EMAIL,
   PASSWORD: process.env.PASSWORD,
   port: process.env.SERVER_PORT,
   API_KEY: process.env.MAILGUN_API_KEY
